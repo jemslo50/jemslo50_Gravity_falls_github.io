e8fe95940b7ba59d662de095ee79be25eba0d32f
jemslo50 <jessy.lorent@student.heaj.be>
2024-05-23 12:55:03 +0200

    Add files via upload

---
+++ début/java scritp/java.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+
+    // Sélectionnez le conteneur de l'image
+    var imageContainer = document.getElementById("imageContainer");
+
+    // Sélectionnez le bouton flèche
+    var arrowButton = document.getElementById("arrowButton");
+
+    function afficherBouton() {
+        document.getElementById("bouton").classList.remove("invisible");
+    }
+});
